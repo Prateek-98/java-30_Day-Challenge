229be56cebb0a93c42803de8cd37840cc3dc4d02
Prateek <prateekjha98@gmail.com>
2022-08-02 21:59:59 +0530

    Subtraction of volumes for Chef

---
+++ Day3.java
@@ -0,0 +1,9 @@
+public class Day3 {
+    public static void main(String[] args) {
+        int reqVol;
+        int avlVol1 = 300;
+        int avlVol2 = 500;
+        reqVol =  avlVol2 - avlVol1;
+        System.out.println(reqVol);
+    }
+}
