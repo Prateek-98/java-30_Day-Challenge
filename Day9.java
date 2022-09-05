214f4128cf15f329bacffb7f0be6b42d9eabd85a
Prateek <prateekjha98@gmail.com>
2022-09-05 19:31:47 +0530

    Calculating Total Cost of gift.

---
+++ Day9.java
@@ -0,0 +1,9 @@
+public class Day9 {
+    public static void main(String[] args) {
+    int sweater = 3 * 68;
+    int computerGame = 75;
+    int bracelets = (2* 43) - 43 + 10;
+    System.out.println("Total cost of Gift is : " + (sweater +computerGame + bracelets));
+    }
+
+}
